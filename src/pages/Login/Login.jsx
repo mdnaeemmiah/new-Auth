2f696fe7b57acae2974c039330add42c58420b09
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-08-25 16:48:10 +0600

    final

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>login</h2>
+        </div>
+    );
+};
+
+export default Login;
